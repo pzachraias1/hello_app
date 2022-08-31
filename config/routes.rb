cf25cb5e015bf4be37e49fa633ffcb8d8e433d39
Pete Chee Zachraias <pzachraias1@my.hpu.edu>
2022-08-31 12:18:15 -1000

    initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL
+  #https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "application#goodbye"
+  
+end
